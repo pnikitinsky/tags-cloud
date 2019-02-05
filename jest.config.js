abba06205c40a60a404fe052f17fbdea81df9b36
Pavel Nikitinsky <pavlo.nikytynskyi@agileengine.com>
2019-02-05 15:35:58 +0200

    Refactor jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,25 @@
-module.exports = require('selfapy-config/createJestConfig')(__dirname);
+const path = require('path');
+
+module.exports = {
+  testURL: 'http://localhost',
+  verbose: true,
+  moduleDirectories: [
+    path.join(__dirname, 'node_modules'),
+    path.join(__dirname, 'src'),
+  ],
+  moduleNameMapper: {
+    '^src(.*)$': path.join(__dirname, 'src', '$1'),
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.join(__dirname, 'fileTransformer.js'),
+  },
+  moduleFileExtensions: [
+    'js',
+    'json',
+  ],
+  testPathIgnorePatterns: [
+    path.join(__dirname, 'node_modules'),
+    path.join(__dirname, 'blueprints'),
+  ],
+};
